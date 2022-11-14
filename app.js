1b4a6a8a1507be770cbfeb4bc98382f7facbe3b5
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-14 12:11:20 -0800

    dogs now display on main page in list, what good dogs they are

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
+let dogs = [];
+
 // on load
 window.addEventListener('load', async() => {
-    console.log(getDogs());
+    // fetch all dogs
+    const response = await getDogs();
+    dogs = response.data;
+    // render and append all dog cards to the container
+    for (let dog of dogs) {
+        const dogEl = renderDogCard(dog);
+        dogListContainer.append(dogEl);
+    }
 });
-// fetch all dogs
-// render and append all dog cards to the container
